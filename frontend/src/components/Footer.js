f64b35f4dabba28f17170c9155b75714214e90f8
steve-ux <exe.universal@gmail.com>
2022-01-07 16:41:06 -0300

    agregado componente cocktails, icono, se corrigio error de ortografia y footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -71,7 +71,7 @@ const Footer = () => {
         <div class="mt-16 border-t-2 border-gray-300 flex flex-col items-center">
           <div class="sm:w-2/3 text-center py-6">
             <p class="text-sm text-blue-700 font-bold mb-2">
-              © 2020 by Pavlove BIOKOU
+              © 2022 by Luxury Drinks
             </p>
           </div>
         </div>
