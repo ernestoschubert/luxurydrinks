8f229059df8cbf9aa88e0403fc2b1c417cf94703
lucas-romer <romerolucas89@gmail.com>
2022-01-07 16:43:29 -0300

    cambios

---
+++ frontend/src/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/main.css";
+import Carousels from "./Carousels";
+
+function Main (){
+    return(
+        <div className="fondoMain">
+            <Carousels/>
+        </div>
+    )
+}
+
+export default Main
