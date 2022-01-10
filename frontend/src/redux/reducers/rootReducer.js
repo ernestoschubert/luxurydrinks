b802dcb0d77a2e42b0950a6c512328a6b27e52d2
ArielSerato <serato.arieli@gmail.com>
2022-01-10 11:31:48 -0300

    feat:Alphabetic filter and price filter

--- frontend/src/redux/reducers/rootReducer.js
+++ frontend/src/redux/reducers/rootReducer.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
 import authReducers from './authReducers';
-
+import productsReducer from './productsReducer';
 const rootReducer = combineReducers({
-    authReducers: authReducers
+    authReducers: authReducers,
+    productsReducer
 })
 
 export default rootReducer;
