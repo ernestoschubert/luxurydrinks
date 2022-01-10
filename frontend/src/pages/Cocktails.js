fd8ed3458ed91d637578cd53269b6c43042ecea1
steve-ux <exe.universal@gmail.com>
2022-01-09 23:06:55 -0300

    avances en page cocktails y page nosotros

--- frontend/src/pages/Cocktails.js
+++ frontend/src/pages/Cocktails.js
@@ -1,13 +1,9 @@
 import Cocktails from "../components/Cocktails";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 
 const Cocktail = () => {
   return (
     <div>
-      <Navbar/>
       <Cocktails/>
-      <Footer/>
     </div>
   );
 };
