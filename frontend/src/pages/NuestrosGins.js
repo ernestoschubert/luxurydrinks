4316df41d021c84250aed61a0789e53713989ef7
Ariel Serato <Serato.arieli@gmail.com>
2022-01-11 10:03:32 -0300

    fix

--- frontend/src/pages/NuestrosGins.js
+++ frontend/src/pages/NuestrosGins.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import Card from "../components/Card";
 import Loader from "../components/Loader";
