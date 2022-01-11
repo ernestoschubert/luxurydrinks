ad262cb1ffe8bd78278d68ada4a38efcb7ebfb9c
unknown <valentinlamothecoulomme@hotmail.com>
2022-01-11 09:36:42 -0300

    panel de admin, falta funcionalidades

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,8 @@ import { withRouter } from "./utils/withRouter";
 import Footer from './components/Footer'
 import SignUp from './pages/SignUp';
 import Login from "./pages/LogIn";
+import PanelAdmin from "./components/panelAdmin";
+
 
 function App() {
   return (
@@ -22,6 +24,7 @@ function App() {
           <Route path="/NuestrosGins" element={<NuestrosGins/>}/>
           <Route path="/Registrarse" element={<SignUp/>}/>
           <Route path="/Loguearse" element={<Login/>}/>
+          <Route path="/panelAdmin" element={<PanelAdmin />} />
         </Routes>
         <Footer/>
       </BrowserRouter>
