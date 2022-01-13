25d962b911307512e60fb960171924f3878b79dd
miguecubilla <cubillamiguel92@gmail.com>
2022-01-13 09:20:59 -0300

    cards carrito

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -4,41 +4,67 @@ const Card = ({ card }) => {
   return (
     <>
     <Link to={`/Gin/${card._id}`} >
-      <div class="wrapper antialiased text-gray-900 ">
-        <div>
-          <img
-            src={card.drinkImg}
-            alt={card.drinkName}
-            class="w-full object-cover object-center rounded-lg shadow-md"
-          />
-          <div class="relative px-4 -mt-16  ">
-            <div class="bg-white p-6 rounded-lg shadow-lg">
-              <div class="flex items-baseline">
-                <span class="bg-teal-200 text-teal-800 text-xs px-2 inline-block rounded-full  uppercase font-semibold tracking-wide">
-                  New
-                </span>
-                <div class="ml-2 text-gray-600 uppercase text-xs font-semibold tracking-wider">
-                  {card.type}
-                </div>
-              </div>
-              <h4 class="mt-1 text-xl font-semibold uppercase leading-tight truncate">
+    <div class="max-w-2xl mx-auto">
+
+
+<div class="bg-luxury shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
+    
+        <img class="rounded-t-lg p-8" src={card.drinkImg} alt={card.drinkName}/>
+   
+    <div class="px-5 pb-5">
+        <a href="#">
+            <h3 class="text-gray-50 font-semibold text-xl tracking-tight dark:text-white">
               {card.drinkName}
-              </h4>
-              <div class="mt-1">
-                ${card.price}
-              </div>
-              <div class="mt-4">
-                <span class="text-teal-600 text-md font-semibold">
-                  Stock:
-                </span>
-                <span class="text-sm text-gray-600">
-                {card.stock}
-                </span>
-              </div>
-            </div>
-          </div>
+              </h3>
+        </a>
+        <div class="flex items-center mt-2.5 mb-5">
+            <svg class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                </path>
+            </svg>
+            <svg class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                </path>
+            </svg>
+            <svg class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                </path>
+            </svg>
+            <svg class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                </path>
+            </svg>
+            <svg class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                </path>
+            </svg>
+            <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
         </div>
-      </div>
+        <div class="flex items-center justify-between">
+            <span class="text-3xl font-bold text-gray-50 dark:text-white">${card.price}</span> <span class="text-sm text-gray-600">
+               stock: {card.stock}
+                </span>
+            <a href="#"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Añadir al carrito</a>
+        </div>
+    </div>
+</div>
+</div>
+
+
+
+
+    
     </Link>
     </>
   );
