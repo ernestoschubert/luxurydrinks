bd83c9ede3865e1c8c15a1ad6c3d079ffbb38d36
ernestoschubert <ernestoschubert23@gmail.com>
2022-01-20 02:45:51 -0300

    feat: agrega el build del front

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ app.use(express.json());
 app.use(passport.initialize());
 app.use("/api", Router);
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname + "/client/build/index.html"));
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
+}
 
 app.listen(process.env.PORT || "4000", "0.0.0.0", () => {
   console.log(`El server esta en el puerto ${process.env.PORT || "4000"}`);
